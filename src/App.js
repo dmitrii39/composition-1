5b20560cd8068b82ea2d81e27a785c02d3fbab19
Дмитрий Цыплов <d.cyplov@yakovlev.studio>
2021-05-12 15:24:10 +0200

    заголовки переданы через props

--- src/App.js
+++ src/App.js
@@ -3,19 +3,25 @@ import './App.css';
 import Card from './components/Card';
 
 const pic = 'https://srisovki.com/wp-content/uploads/2018/12/krasiv-i-legk-9.jpg';
+
+
 function App() {
 
   return (
     <div className="App-header">
-      <Card>
-       <img src={pic} class="card-img-top" alt="..."></img> 
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text txt">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <Card title={'Card title'}>
+       <img src={pic} className="card-img-top" alt="..."></img> 
+       
+        <p className="card-text txt">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     
       </Card>
-      <Card >
-        <h5 class="card-title">Spacial title treatment</h5>
-        <p class="card-text txt">With supporting text below as a natural lead-in to additional content</p>
+
+
+
+
+      <Card title={'Spacial title treatment'}>
+      
+        <p className="card-text txt">With supporting text below as a natural lead-in to additional content</p>
       </Card>
       
     </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,21 @@
-// import logo from './logo.svg';
+
 import '../App.css';
 
 function Card(props) {
   return (
-      <div class="card card_img">
+      <div className="card card_img">
           
-  <div class="card-body">
-      {props.children}
-    <a href="https://www.ejin.ru/wp-content/uploads/2017/09/4-597.jpg" class="btn btn-primary">Go somewhere</a>
+  <div className="card-body">
+       
+       {props.children[0]}
+      <h5 className="card-title">{props.title}</h5>
+      {props.children.length === 2 ?  props.children[1] : props.children}
+    
+    <a href="https://www.ejin.ru/wp-content/uploads/2017/09/4-597.jpg" className="btn btn-primary">Go somewhere</a>
   </div>
+  {console.log(props.children)}
       </div>
-   
+    
   );
 }
 
