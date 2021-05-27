7991e3c2e291f21a72e9b32da519a4215fe6c99e
Дмитрий Цыплов <d.cyplov@yakovlev.studio>
2021-05-28 00:33:09 +0200

    fix & debug

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
-// import logo from './logo.svg';
-import './App.css';
-import Card from './components/Card';
+import kotik from "./kotik.jpg";
+import "./App.css";
+import Card from "./components/Card";
 
-const pic = 'https://srisovki.com/wp-content/uploads/2018/12/krasiv-i-legk-9.jpg';
-
+const txt1 =
+  "Some quick example text to build on the card title and make up the bulk of the card's content.";
+const txt2 =
+  "With supporting text below as a natural lead-in to additional content";
 
 function App() {
-
   return (
     <div className="App-header">
-      <Card title={'Card title'}>
-       <img src={pic} className="card-img-top" alt="..."></img> 
-       
-        <p className="card-text txt">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    
+      <Card title={"Card title"} img={kotik}>
+        <p className="card-text txt">{txt1}</p>
       </Card>
-
-
-
-
-      <Card title={'Spacial title treatment'}>
-      
-        <p className="card-text txt">With supporting text below as a natural lead-in to additional content</p>
+      <Card title={"Spacial title treatment"} img={null}>
+        <p className="card-text txt">{txt2}</p>
       </Card>
-      
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,23 @@
-
-import '../App.css';
+import "../App.css";
 
 function Card(props) {
   return (
-      <div className="card card_img">
-          
-  <div className="card-body">
-       
-       {props.children[0]}
-      <h5 className="card-title">{props.title}</h5>
-      {props.children.length === 2 ?  props.children[1] : props.children}
-    
-    <a href="https://www.ejin.ru/wp-content/uploads/2017/09/4-597.jpg" className="btn btn-primary">Go somewhere</a>
-  </div>
-  {console.log(props.children)}
+    <div className="card card_img">
+      <div className="card-body">
+        {props.img && (
+          <img src={props.img} className="card-img-top" alt="foto" />
+        )}
+        <h5 className="card-title">{props.title}</h5>
+        {props.children}
+
+        <a
+          href="https://www.ejin.ru/wp-content/uploads/2017/09/4-597.jpg"
+          className="btn btn-primary"
+        >
+          Go somewhere
+        </a>
       </div>
-    
+    </div>
   );
 }
 
